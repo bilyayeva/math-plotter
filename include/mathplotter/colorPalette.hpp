265509d8517b0cf467129def0b2c5a321aa16759
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-30 01:41:14 +0200

    feat(colorPalette): add base colors

--- include/mathplotter/colorPalette.hpp
+++ include/mathplotter/colorPalette.hpp
@@ -1,4 +1,18 @@
 #ifndef MATHPLOTTER_COLOR_PALETTE_HPP
 #define MATHPLOTTER_COLOR_PALETTE_HPP
 
+#include <SFML/Graphics/Color.hpp>
+
+namespace mathplotter {
+    
+    struct ColorPalette {
+        static constexpr sf::Color kBlack    {0, 0, 0};
+        static constexpr sf::Color kWhite    {255, 255, 255};
+        static constexpr sf::Color kRed      {255, 0, 0};
+        static constexpr sf::Color kGreen    {0, 255, 0};
+        static constexpr sf::Color kBlue     {0, 0, 255};
+    };
+
+} // namespace mathplotter
+
 #endif
