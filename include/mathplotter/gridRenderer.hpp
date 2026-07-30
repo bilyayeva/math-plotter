974913fc7074da5fa5ab31583093868c7775547d
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-30 22:20:50 +0200

    feat(gridRenderer): add grid renderer class declaration

--- include/mathplotter/gridRenderer.hpp
+++ include/mathplotter/gridRenderer.hpp
@@ -1,4 +1,16 @@
 #ifndef MATHPLOTTER_GRID_RENDERER_HPP
 #define MATHPLOTTER_GRID_RENDERER_HPP
 
+namespace mathplotter {
+
+    class GridRenderer {
+    private:
+        float m_majorGridStep;
+        float m_minorGridStep;
+    public:
+        explicit GridRenderer(float majorGridStep); 
+    };
+
+} // namespace mathplotter
+
 #endif
