393a92e3135d420ba943eb6ec035edf1063e1753
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-30 23:32:08 +0200

    feat(application): add application class declaration

--- include/mathplotter/application.hpp
+++ include/mathplotter/application.hpp
@@ -1,4 +1,34 @@
 #ifndef MATHPLOTTER_APPLICATION_HPP
 #define MATHPLOTTER_APPLICATION_HPP
 
+#include <mathplotter/cameraController.hpp>
+#include <mathplotter/functionRenderer.hpp>
+#include <mathplotter/gridRenderer.hpp>
+#include <mathplotter/theme.hpp>
+#include <mathplotter/userInterface.hpp>
+
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/View.hpp>
+
+#include <vector>
+
+namespace mathplotter {
+
+    class Application {
+    private:
+        sf::RenderWindow                m_window;
+        sf::Font                        m_font;
+        sf::View                        m_interfaceView;
+        Theme                           m_theme;
+        CameraController                m_cameraController;
+        GridRenderer                    m_gridRenderer;
+        std::vector<FunctionRenderer>   m_functionRenderers;
+        UserInterface                   m_userInterface;
+    public:
+        Application();
+    };
+
+} // namespace mathplotter
+
 #endif
