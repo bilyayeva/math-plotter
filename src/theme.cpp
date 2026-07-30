fbeb1bba56ead1b7d83515fc4601ff15ff6ef9a8
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-30 21:44:20 +0200

    feat(theme): implement theme initialization

---
+++ src/theme.cpp
@@ -0,0 +1,29 @@
+#include <mathplotter/theme.hpp>
+#include <mathplotter/colorPalette.hpp>
+
+#include <stdexcept>
+
+namespace mathplotter {
+
+    Theme::Theme(ThemeType type) : m_type(type) {
+        switch (type) {
+            case ThemeType::Light:
+                m_backgroundColor = ColorPalette::kWhite;
+                m_majorGridColor  = ColorPalette::kGreen;
+                m_minorGridColor  = ColorPalette::kBlue;
+                m_axisColor       = ColorPalette::kBlack;
+                m_axisLabelColor  = ColorPalette::kBlack;
+                break;
+            case ThemeType::Dark:
+                m_backgroundColor = ColorPalette::kBlack;
+                m_majorGridColor  = ColorPalette::kRed;
+                m_minorGridColor  = ColorPalette::kGreen;
+                m_axisColor       = ColorPalette::kWhite;
+                m_axisLabelColor  = ColorPalette::kWhite;
+                break;
+            default:
+                throw std::invalid_argument("Theme: Unsupported theme type.");
+        }
+    }
+
+} // namespace mathplotter
