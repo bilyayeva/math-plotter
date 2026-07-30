0bba1640c68283dcd5c92c4c947e508594f3a95e
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-30 21:26:27 +0200

    feat(theme): add theme class declaration

--- include/mathplotter/theme.hpp
+++ include/mathplotter/theme.hpp
@@ -1,4 +1,27 @@
 #ifndef MATHPLOTTER_THEME_HPP
 #define MATHPLOTTER_THEME_HPP
 
+#include <SFML/Graphics/Color.hpp>
+
+namespace mathplotter {
+    
+    enum class ThemeType {
+        Light,
+        Dark
+    };
+
+    class Theme {
+    private:
+        ThemeType m_type;
+        sf::Color m_backgroundColor;
+        sf::Color m_majorGridColor;
+        sf::Color m_minorGridColor;
+        sf::Color m_axisColor;
+        sf::Color m_axisLabelColor;
+    public:
+        explicit Theme(ThemeType type);
+    };
+
+} // namespace mathplotter
+
 #endif
