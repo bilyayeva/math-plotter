2016fde59f28ade41f86cd32f7e78ea9c54e00db
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-30 22:41:44 +0200

    feat(functionRenderer): add function renderer class declaration

--- include/mathplotter/functionRenderer.hpp
+++ include/mathplotter/functionRenderer.hpp
@@ -1,4 +1,17 @@
 #ifndef MATHPLOTTER_FUNCTION_RENDERER_HPP
 #define MATHPLOTTER_FUNCTION_RENDERER_HPP
 
+#include <string>
+
+namespace mathplotter {
+
+    class FunctionRenderer {
+    private:
+        std::string m_expression;
+    public:
+        explicit FunctionRenderer(std::string expression);
+    };
+
+} // namespace mathplotter
+
 #endif
