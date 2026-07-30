637e672435f4ed0adc3a42ca8e5a8ac428142529
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-30 23:03:13 +0200

    feat(userInterface): add user interface class declaration

--- include/mathplotter/userInterface.hpp
+++ include/mathplotter/userInterface.hpp
@@ -1,4 +1,18 @@
 #ifndef MATHPLOTTER_USER_INTERFACE_HPP
 #define MATHPLOTTER_USER_INTERFACE_HPP
 
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Text.hpp>
+
+namespace mathplotter {
+
+    class UserInterface {
+    private:
+        sf::Text m_title;
+    public:
+        explicit UserInterface(const sf::Font& font);
+    };
+
+} // namespace mathplotter
+
 #endif
