97c8c744c693339629bc7a9c74381fbbac0fa4f9
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-30 22:00:31 +0200

    feat(cameraController): add camera controller class declaration

--- include/mathplotter/cameraController.hpp
+++ include/mathplotter/cameraController.hpp
@@ -1,4 +1,18 @@
 #ifndef MATHPLOTTER_CAMERA_CONTROLLER_HPP
 #define MATHPLOTTER_CAMERA_CONTROLLER_HPP
 
+#include <SFML/Graphics/View.hpp>
+
+namespace mathplotter {
+
+    class CameraController {
+    private:
+        sf::View m_view;
+        float    m_zoomLevel;
+    public:
+        explicit CameraController(sf::View view);
+    };
+
+} // namespace mathplotter
+
 #endif
