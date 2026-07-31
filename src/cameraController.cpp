fc709c26b980c84f0900c06e6107c069649b226a
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-31 16:56:11 +0200

    feat(cameraController): center view at world origin

--- src/cameraController.cpp
+++ src/cameraController.cpp
@@ -5,6 +5,7 @@ namespace mathplotter {
     CameraController::CameraController(sf::View view)
         : m_view(view),
           m_zoomLevel(1.0f) {
+        m_view.setCenter({0.f, 0.f});
     }
 
 } // namespace mathplotter
