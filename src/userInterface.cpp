dcedfa3f2ef0a0c79f17e802a487757d5c3b2246
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-31 14:53:55 +0200

    feat(userInterface): implement draw method

--- src/userInterface.cpp
+++ src/userInterface.cpp
@@ -1,11 +1,16 @@
 #include <mathplotter/userInterface.hpp>
 
 #include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 
 namespace mathplotter {
 
     UserInterface::UserInterface(const sf::Font& font)
         : m_title(font, "Math Plotter++") {
+    }
+
+    void UserInterface::Draw(sf::RenderWindow& window) const {
+        window.draw(m_title);
     }
 
 } // namespace mathplotter
