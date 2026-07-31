07953e74e7597518de3836f69d616daabfa2b7f0
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-31 15:17:00 +0200

    feat(theme): add title outline color getter declaration

--- include/mathplotter/theme.hpp
+++ include/mathplotter/theme.hpp
@@ -23,6 +23,7 @@ namespace mathplotter {
     public:
         explicit Theme(ThemeType type);
         sf::Color GetTitleFillColor() const;
+        sf::Color GetTitleOutlineColor() const;
     };
 
 } // namespace mathplotter
