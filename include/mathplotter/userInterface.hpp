c93013b7c9aa41e7ef1fa78d169cf50ce6202421
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-31 15:56:42 +0200

    feat(userInterface): add title configuration method

--- include/mathplotter/userInterface.hpp
+++ include/mathplotter/userInterface.hpp
@@ -4,6 +4,7 @@
 #include <mathplotter/theme.hpp>
 
 #include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Text.hpp>
 
@@ -12,6 +13,8 @@ namespace mathplotter {
     class UserInterface {
     private:
         sf::Text m_title;
+
+        void ConfigureTitle(const Theme& theme);
     public:
         UserInterface(const sf::Font& font, const Theme& theme);
         void Draw(sf::RenderWindow& window) const;
--- src/userInterface.cpp
+++ src/userInterface.cpp
@@ -2,15 +2,24 @@
 #include <mathplotter/userInterface.hpp>
 
 #include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 
 namespace mathplotter {
 
     UserInterface::UserInterface(const sf::Font& font, const Theme& theme)
         : m_title(font, "Math Plotter++") {
+        ConfigureTitle(theme);
+    }
+
+    void UserInterface::ConfigureTitle(const Theme& theme) {
         m_title.setFillColor(theme.GetTitleFillColor());
         m_title.setOutlineColor(theme.GetTitleOutlineColor());
         m_title.setOutlineThickness(1.f);
+        m_title.setCharacterSize(20u);
+        const sf::FloatRect titleBounds{m_title.getLocalBounds()};
+        m_title.setOrigin(titleBounds.position);
+        m_title.setPosition({5.f, 5.f});
     }
 
     void UserInterface::Draw(sf::RenderWindow& window) const {
