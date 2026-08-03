03d18f3319221b305f940cd211e9e1b41b29fa25
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-08-03 23:15:30 +0200

    feat(colorPalette): add new colors

--- include/mathplotter/colorPalette.hpp
+++ include/mathplotter/colorPalette.hpp
@@ -6,11 +6,13 @@
 namespace mathplotter {
     
     struct ColorPalette {
-        static constexpr sf::Color kBlack    {0, 0, 0};
-        static constexpr sf::Color kWhite    {255, 255, 255};
-        static constexpr sf::Color kRed      {255, 0, 0};
-        static constexpr sf::Color kGreen    {0, 255, 0};
-        static constexpr sf::Color kBlue     {0, 0, 255};
+        static constexpr sf::Color kBlack       {0, 0, 0};
+        static constexpr sf::Color kWhite       {255, 255, 255};
+        static constexpr sf::Color kRed         {255, 0, 0};
+        static constexpr sf::Color kGreen       {0, 255, 0};
+        static constexpr sf::Color kBlue        {0, 0, 255};
+        static constexpr sf::Color kGrey        {150, 150, 150};
+        static constexpr sf::Color kLightGrey   {220, 220, 220};
     };
 
 } // namespace mathplotter
